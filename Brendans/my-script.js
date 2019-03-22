8eeed4bf21e830bb39de20eea899c998ee9b9fde
Jason Harris <jason.harris@endsleigh.co.uk>
2019-03-22 10:10:19 +0000

    Update the newsletter link

--- Brendans/my-script.js
+++ Brendans/my-script.js
@@ -2,6 +2,9 @@ $(function() {
   $('#js-contact-form').submit(function (event) {
     $('#js-contact-form').hide();
     $('#js-contact-thankyou').show();
+
+    // https://501n7ggn65.execute-api.eu-west-1.amazonaws.com/dev
+    
     event.preventDefault();
   });
 });
